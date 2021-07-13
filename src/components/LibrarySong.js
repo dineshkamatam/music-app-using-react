b6f319a81d72d0f7f7b0927ac0bc8b5a403a340a
dineshkamatam <dineshkamatam25@gmail.com>
2021-07-13 15:01:54 +0530

    added all functionalities

---
+++ src/components/LibrarySong.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const LibrarySong = ({
+  song,
+  songs,
+  setCurrentSong,
+  id,
+  audioRef,
+  isPlaying,
+  setSongs,
+}) => {
+  const songSelectHandler = async () => {
+    const selectedSong = songs.filter((state) => state.id === song.id);
+    await setCurrentSong(selectedSong[0]);
+    //Add Active song
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+    // audioRef.current.play();
+    if (isPlaying) audioRef.current.play();
+  };
+
+  return (
+    <div
+      className={`library-song ${song.active ? "selected" : ""}`}
+      onClick={songSelectHandler}
+    >
+      <img src={song.cover} alt={song.name} />
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
---
+++ src/data.js
@@ -0,0 +1,198 @@
+import {v4 as uuidv4} from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Simplexity",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/04/cb0cc6270d7f2e1bb13e44e8832bd5c9b2a61080-1024x1024.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=14958",
+      color: ["#EC6E51", "#E5AF4F"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Home Court",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
+      artist: "Blue Wednesday, Shopan",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11233",
+      color: ["#391511", "#F8BB7D"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "On my mind",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/2c3bd458bfb0713c89f991d1ce469523e95e3b53-1024x1024.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=1439",
+      color: ["#91569E", "#F08B67"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Foggy Road",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/efaa848553d09b4d9fc0752844456e41b34de276-1024x1024.jpg",
+      artist: "Toonorth",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7834",
+      color: ["#113437", "#56A1F1"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Home Court",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
+      artist: "Blue Wednesday, Shopan",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11233",
+      color: ["#391511", "#F8BB7D"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "On my mind",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/2c3bd458bfb0713c89f991d1ce469523e95e3b53-1024x1024.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=1439",
+      color: ["#91569E", "#F08B67"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Foggy Road",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/efaa848553d09b4d9fc0752844456e41b34de276-1024x1024.jpg",
+      artist: "Toonorth",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7834",
+      color: ["#113437", "#56A1F1"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Home Court",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
+      artist: "Blue Wednesday, Shopan",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11233",
+      color: ["#391511", "#F8BB7D"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "On my mind",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/2c3bd458bfb0713c89f991d1ce469523e95e3b53-1024x1024.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=1439",
+      color: ["#91569E", "#F08B67"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Foggy Road",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/efaa848553d09b4d9fc0752844456e41b34de276-1024x1024.jpg",
+      artist: "Toonorth",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7834",
+      color: ["#113437", "#56A1F1"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Home Court",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
+      artist: "Blue Wednesday, Shopan",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11233",
+      color: ["#391511", "#F8BB7D"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "On my mind",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/2c3bd458bfb0713c89f991d1ce469523e95e3b53-1024x1024.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=1439",
+      color: ["#91569E", "#F08B67"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Foggy Road",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/efaa848553d09b4d9fc0752844456e41b34de276-1024x1024.jpg",
+      artist: "Toonorth",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7834",
+      color: ["#113437", "#56A1F1"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Home Court",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
+      artist: "Blue Wednesday, Shopan",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11233",
+      color: ["#391511", "#F8BB7D"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "On my mind",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/2c3bd458bfb0713c89f991d1ce469523e95e3b53-1024x1024.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=1439",
+      color: ["#91569E", "#F08B67"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Foggy Road",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/efaa848553d09b4d9fc0752844456e41b34de276-1024x1024.jpg",
+      artist: "Toonorth",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7834",
+      color: ["#113437", "#56A1F1"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Home Court",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
+      artist: "Blue Wednesday, Shopan",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11233",
+      color: ["#391511", "#F8BB7D"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "On my mind",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/2c3bd458bfb0713c89f991d1ce469523e95e3b53-1024x1024.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=1439",
+      color: ["#91569E", "#F08B67"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Foggy Road",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/efaa848553d09b4d9fc0752844456e41b34de276-1024x1024.jpg",
+      artist: "Toonorth",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7834",
+      color: ["#113437", "#56A1F1"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
